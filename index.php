aa7c7dc8eb0c356905b9751f20953898ddeb5430
IlyaPer <62990147+IlyaPer@users.noreply.github.com>
2020-08-24 11:01:36 +0300

    Первая разметка

--- index.php
+++ index.php
@@ -1,51 +1,39 @@
-<?php
-error_reporting(-1);
-ini_set('display_errors','On');
-header("Content-Type: text/html; charset=utf-8");
-
-if(isset($_POST['login'],$_POST['password'])) {
-    if($_POST['login'] === 'admin' && $_POST['password'] === '123') {
-        header("we");
-        exit;
-    } else {
-        $error = 'Вы неправильно ввели логин или пароль';
-    }
-}
-?>
-
-<!doctype html>
-<html>
-<head>
-<link href="style.css" rel="stylesheet" type="text/css">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Y-Engine</title>
-</head>
-<body>
-
-<?php
-error_reporting(-1);
-ini_set('display_errors','On');
-header("Content-Type: text/html; charset=utf-8");
-
-if(isset($_POST['login'],$_POST['password'])) {
-    if($_POST['login'] === 'admin' && $_POST['password'] === '123') {
-        header("we");
-        exit;
-    } else {
-        $error = 'Вы неправильно ввели логин или пароль';
-    }
-}
-?>
-<?php if(isset($error)) { ?>
-    <div>Здравствуйте, <?=htmlspecialchars($error);?></div>
-<?php } ?>
-<form action="main.php" method="post">
-    <input type="text" name="login" placeholder="Введите логин">
-    <input type="password" name="password" placeholder="введите пароль">
-    <input type="submit" value="Войти на сайт!">
-</form>
-</body>
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+        <link href="main.css" rel="stylesheet">
+        <title>Banji </title>
+	</head>
+	<body>
+        <input type="checkbox" id="hmt" class="hidden-menu-ticker">
+            <label class="btn-menu" for="hmt">
+            <span class="first"></span>
+            <span class="second"></span>
+            <span class="third"></span>
+        </label>
+            <ul class="hidden-menu">
+                <li><a href="main.html" class="menu-text">Главная</a></li>
+                <li><a href="page.html" class="menu-text">Мой профиль</a></li>
+                <li><a href="" class="menu-text">Задачи</a></li>
+                <li><a href="" class="menu-text">Вакансии</a></li>
+                <li><a href="" class="menu-text">Общение</a></li>
+                <li><a href="" class="menu-text">Настройки</a></li>
+        </ul>
+        <h1 class="w1">Главная</h1>
+        <h2 class="w1">Готовы начать?</h2>
+        <form>
+        <input type="text" placeholder="Поиск по проектам">
+        <button type="submit"></button>
+        </form>
+						<ul class="project_list">
+							<li class=""> 
+									<img src="shrek.jpeg">
+									<div class="project_info">
+								   		<p class="project_category">Тестовая категория</p>
+									 		<h3 class="project_name"><a href=" Здесь должна быть ссылка на страничку проекта ">Тестовый проект</a></h3>
+									 		<p class="project_description">Жил да был в сказочном государстве большой зеленый великан по имени Шрек. Жил он в гордом одиночестве в лесу, на болоте, которое считал своим. Но однажды злобный коротышка — лорд Фаркуад, правитель волшебного королевства, безжалостно согнал на Шреково болото всех сказочных обитателей.</p>
+									</div>
+							</li>
+						</ul>
+    </body>
 </html>
-<?php
-    echo 'test1';
- ?>
