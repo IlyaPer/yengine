109ed073daf80217c9885208a83d8525c1f5d87e
IlyaPer <62990147+IlyaPer@users.noreply.github.com>
2020-08-28 20:10:07 +0300

    Последние изменения

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
   $con = mysqli_connect("localhost", "root", "", "yeticave");
   if ($con == false) {
-    print("Ошибка подключения: " . mysqli_connect_error());
+    exit;
   }
-  else {
-    print("Соединение установлено");
     mysqli_set_charset($con, "utf8");
-    $sql_lots = "SELECT categories.name, lots.name, first_price, url, date_delection, bet_step FROM lots JOIN categories ON categories.id = lots.category_id WHERE date_delection > NOW();";
+    $sql_cat = "SELECT name, id, img FROM categories;";
+    $result_cat = mysqli_query($con, $sql_cat);
+    if (!$result_cat) {
+      exit;
+    }
+    $categories = mysqli_fetch_all($result_cat, MYSQLI_ASSOC);
+    $sql_lots = "SELECT  categories.name AS category_name, lots.name, lots.id AS lot_url, first_price, url, date_delection, bet_step FROM lots JOIN categories ON categories.id = lots.category_id WHERE date_delection > NOW() ORDER BY date_delection DESC;";
     $result_lots = mysqli_query($con, $sql_lots);
     if (!$result_lots) {
-      $error = mysqli_error($con);
-      print("Ошибка MySQL: " . $error);
+      exit;
     }
     $products = mysqli_fetch_all($result_lots, MYSQLI_ASSOC);
 
-    $sql_cat = "SELECT name FROM categories;";
-    $result_cat = mysqli_query($con, $sql_cat);
-    if (!$result_cat) {
-      $error = mysqli_error($con);
-      print("Ошибка MySQL: " . $error);
-    }
-    $categories = mysqli_fetch_all($result_cat, MYSQLI_ASSOC);
-
-  }
       require_once("function.php");
       require_once("helpers.php");
 
--- main.php
+++ main.php
@@ -1,99 +1,53 @@
-
-<! DOCTYPE HTML>
-<html>
-<head>
-<link href="style.css" rel="stylesheet" type="text/css">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Y-Engine</title>
-</head>
-<body>
-    <div class="bg">
-       <ul>
-           <a href="https://google.ru"><li class="li1">Главная страница</li></a>
-           <a href="index2.php"><li class="li1">Регистрация</li></a>
-           <a href="loading.php"><li class="li1">Страница загрузки проекта</li></a>
-           <a href=""><li class="li1">Профиль</li></a>
-           <a href="support.php"><li class="li1">тех поддержка</li></a>
-       </ul>
-    </div>
-  
-<form action="action.php" method="post">
- <p>Ваше имя: <input type="text" name="name" /></p>
- <p>Ваш возраст: <input type="text" name="age" /></p>
- <p><input type="submit" /></p>
-</form>
-<hr>
-<h1 class="h1">Banji</h1>
-<p class="p12">Быть впереди.</p>
-<form action="index.html">
-<div class="container">
-<h1>Register</h1>
-<p>Please fill in this form to create an account.</p>
-<hr>
-
-<label for="email"><b>Email</b></label>
-<input type="text" placeholder="Enter Email" name="email" required>
-
-<label for="psw"><b>Password</b></label>
-<input type="password" placeholder="Enter Password" name="psw" required>
-
-<label for="psw-repeat"><b>Repeat Password</b></label>
-<input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-<hr>
-
-<p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-<button type="submit" class="registerbtn">Register</button>
-</div>
-
-<div class="container signin">
-<p>Already have an account? <a href="#">Sign in</a>.</p>
-</div>
-</form>
-<div class="hello">
-<div class="card">
-<img src="images/pn43K4opZM0.jpg" alt="Avatar">
-<div class="container">
-<h4><b>Мировой охват</b></h4>
-<p class="e1">Architect & Engineer</p>
-</div>
-</div>
-<div class="card2">
-<img src="images/cNJ6OPCnxC8.jpg" alt="Avatar">
-<div class="container">
-<h4><b>Развитие на любой стадии</b></h4>
-<p class="e1">Architect & Engineer</p>
-</div>
-</div>
-<div class="card3">
-<img src="images/FiMlHZeiNDs.jpg" alt="Avatar">
-<div class="container">
-<h4><b>Удобный интерфейс</b></h4>
-<p class="e1">Architect & Engineer</p>
-</div>
-</div>
-<div class="card4">
-<img src="images/4rjUOQfr8Hs.jpg" alt="Avatar">
-<div class="container">
-<h4><b>Походит каждому</b></h4>
-<p class="e1">Architect & Engineer</p>
-</div>
-</div>
-</div>
-<h2 class="w2">твоеимя.engine</h2>
-<p class="w3">Крупнейшая социальная сеть для стартаперов и инвесторов.</p>
-<div class="rotate-shadows3"></div>
-<h1 class="w3">Много функций для стартапа на любой стадии.</h1>
-<p>Собирайте средства на свой проект при помощи технологии краудфандинга.</p>
-<p class="fig"><img src="images/images.jpg" alt="альтернативный текст" ></p>
-<p>Создавайте потрясающие лэндинги моментально, визуализируйте ваши идеи пррямо сейчас.</p>
-<h2 class="w4">Создавайте потрясающие лэндинги моментально, визуализируйте ваши идеи пррямо сейчас. <br>Находите единомышленников с помощью умной системы отбора.</h2>
-<h2 class="q1">Привлекайте ваших первых пользователей.</h2>
-<p class="fig"><img src="images/images-3.jpg" alt="альтернативный текст" ></p>
-<h2 class="q2">Делитесь своими идеями с помощью специальной системы защиты от плагиата.</h2>
-<p class="fig"><img src="images/HELLO.png" alt="альтернативный текст" ></p>
-<h2 class="q3">30 дней. Бесплатно.</h2>
-</body>
-</html>
-<?php
-    echo 'test123';
- ?>
+<main class="container">
+    <section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <?php
+                foreach($categories as $category):
+               ?>
+            <li class="promo__item promo__item--<?=$category['img']; ?>">
+                    <a class="promo__link" href="pages/all-lots.html"><?=$category['name']; ?></a>
+              </li>
+              <?php endforeach; ?>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+            <?php
+              foreach ($products as $product):
+              $result_time = deletion_of_lot($product['date_delection']);
+            ?>
+            <li class="lots__item lot">
+              <div class="image">
+                    <img src="<?=$product['url']; ?>" width="350" height="260" alt="">
+              </div>
+              <div class="lot__info">
+                <?php
+                ?>
+                 <span class="lot__category"><?=$product['category_name'];?></span>
+                  <h3 class="lot__title">  <a class="text-link" href="lot.php<?= "?id=" . $product['lot_url'];?>"><?= htmlspecialchars($product['name']);?></a></h3>
+                  <div class="lot__state">
+                      <div class="lot__rate">
+                          <span class="lot__amount">Стартовая цена</span>
+                         <span class="lot__cost"> <?= htmlspecialchars(format_sum($product['first_price'])); ?></span>
+                      </div>
+                      <div class="lot__timer timer <?php
+                      if ($result_time[0] < 1){
+                     echo ' timer--finishing';
+                   };
+                       ?>"
+                       >
+                          <?=  $rest_time = implode(":", $result_time);
+                          ?>
+                      </div>
+                    </div>
+                </div>
+            </li>
+               <?php endforeach; ?>
+        </ul>
+    </section>
+</main>
